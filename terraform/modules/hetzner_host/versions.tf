99f6a1750d83d9ee5dbfb08b77b36fabf5f627aa
Lukas Güldenstein <git@lukasgueldenstein.de>
2024-08-19 21:18:13 +0200

    cleanup

--- terraform/modules/hetzner_host/versions.tf
+++ terraform/modules/hetzner_host/versions.tf
@@ -4,9 +4,5 @@ terraform {
     hcloud = {
       source = "hetznercloud/hcloud"
     }
-    hetznerdns = {
-      source  = "timohirt/hetznerdns"
-      version = ">= 1.1.1"
-    }
   }
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,9 +6,5 @@ terraform {
       source  = "hetznercloud/hcloud"
       version = ">= 1.44.1"
     }
-    hetznerdns = {
-      source  = "timohirt/hetznerdns"
-      version = ">= 2.2.0"
-    }
   }
 }
